450f8179659c081f8ce8e8164d3a62178ea3cb6e
longhdwst1full <106103998+longhdwst1full@users.noreply.github.com>
2023-02-11 09:26:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Navbar() {
+    return (
+
+        <div className="h-24 bg-slate-900  text-center text-white leading-[6rem] text-4xl">
+            <i className="fa-brands fa-spotify mr-5"></i>
+            Spotyfi
+        </div>
+    )
+}
